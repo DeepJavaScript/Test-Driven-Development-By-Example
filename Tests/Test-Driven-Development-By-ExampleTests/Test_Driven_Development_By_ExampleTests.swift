ff072ada802aec1ad4709aa3c28817e673573a3d
Tsungyu Yu <ytyubox@gmail.com>
2022-06-05 19:55:25 +0800

    groject init

---
+++ Tests/Test-Driven-Development-By-ExampleTests/Test_Driven_Development_By_ExampleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Test_Driven_Development_By_Example
+
+final class Test_Driven_Development_By_ExampleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Test_Driven_Development_By_Example().text, "Hello, World!")
+    }
+}
