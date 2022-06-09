17fd1003052ecd1e24976c3728269dc758afec06
gene_lin <marioreda3@gmail.com>
2022-06-09 09:21:22 +0800

    ch01-MultiCurrencyMoney 1.增加紅燈測試 計算2倍

---
+++ Gene/TDD-By-Example/Part1-Money/01-MultiCurrencyMoney.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDD_By_Example
+{
+    [TestClass]
+    public class MultiCurrencyMoney
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.Amount);
+        }
+    }
+}
---
+++ Gene/TDD-By-Example/Part1-Money/Dollar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_By_Example
+{
+    class Dollar
+    {
+        public int Amount;
+
+        public Dollar(int amount)
+        {
+            
+        }
+
+        public void Times(int multiplier)
+        {
+            
+        }
+    }
+}
