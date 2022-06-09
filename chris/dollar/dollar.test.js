5a2358e0f463134078f2ee8e207e9439e4a74e84
chris <dwatow@gmail.com>
2022-06-09 20:21:02 +0800

    [x] modify test for new interface of Dollar

--- chris/dollar/dollar.test.js
+++ chris/dollar/dollar.test.js
@@ -5,9 +5,9 @@ describe('normal features', () => {
   it('test multiplication', () => {
     // prints name of the test
     const five = new Dollar(5)
-    five.times(2)
-    assert.equal(10, five.amount)
-    five.times(3)
-    assert.equal(15, five.amount)
+    let product = five.times(2);
+    assert.equal(10, product.amount)
+    product = five.times(3)
+    assert.equal(15, product.amount)
   })
 })
