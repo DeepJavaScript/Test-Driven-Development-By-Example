8dd0a1b6a4da1a933dea529dfb375cab21de6242
gene_lin <marioreda3@gmail.com>
2022-06-09 09:21:52 +0800

    ch02-DegenerateObjects 1.增加紅燈測試：分別進行2倍與3倍運算

---
+++ Gene/TDD-By-Example/Part1-Money/02-DegenerateObjects.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TDD_By_Example.Part1_Money
+{
+    [TestClass]
+    public class DegenerateObjects
+    {
+        [TestMethod]
+        public void TestMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.Amount);
+            five.Times(3);
+            Assert.AreEqual(15, five.Amount);
+        }
+    }
+}
