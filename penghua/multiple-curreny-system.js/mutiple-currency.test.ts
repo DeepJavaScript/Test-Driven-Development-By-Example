cc4efc3c7d474550281ae737861f46ed7071bc2c
penghuachen <kids5346@gmail.com>
2022-06-09 20:06:10 +0800

    feat: add test but will run fails because it is not initialize Dollar object and times method

---
+++ penghua/multiple-curreny-system.js/mutiple-currency.test.ts
@@ -0,0 +1,9 @@
+describe("testMultiplication", () => {
+    test("testMultiplication", () => {
+        const five = new Dollar(5);
+        five.times(2);
+
+        expect(five.times(2)).toBe(10);
+    })
+})
+
