0b85cec915c2006ff2dfd9a360c5f337fe6e6497
Tsungyu Yu <ytyubox@gmail.com>
2022-06-12 17:46:57 +0800

    ❌ API design:Bank + Expression

--- Yu/Tests/Test-Driven-Development-By-ExampleTests/Test_Driven_Development_By_ExampleTests.swift
+++ Yu/Tests/Test-Driven-Development-By-ExampleTests/Test_Driven_Development_By_ExampleTests.swift
@@ -1,31 +1,36 @@
 import Test_Driven_Development_By_Example
 import XCTest
 
-
 final class WyCachTests: XCTestCase {
-  func testMulitplication() throws {
-      let five = Money.dollar(5)
-    XCTAssertEqual(Money.dollar(10), five.times(2))
-    XCTAssertEqual(Money.dollar(15), five.times(3))
-  }
+    func testMulitplication() throws {
+        let five = Money.dollar(5)
+        XCTAssertEqual(Money.dollar(10), five.times(2))
+        XCTAssertEqual(Money.dollar(15), five.times(3))
+    }
 
-  func testFrancMulitplication() throws {
-      let five = Money.franc(5)
-      XCTAssertEqual(Money.franc(10), five.times(2))
-      XCTAssertEqual(Money.franc(15), five.times(3))
-  }
+    func testFrancMulitplication() throws {
+        let five = Money.franc(5)
+        XCTAssertEqual(Money.franc(10), five.times(2))
+        XCTAssertEqual(Money.franc(15), five.times(3))
+    }
 
-  func testEquality() throws {
-      XCTAssertEqual(Money.dollar(5), Money.dollar(5))
-      XCTAssertNotEqual(Money.dollar(5), Money.dollar(6))
-      XCTAssertNotEqual(Money.franc(5), Money.dollar(5))
-  }
+    func testEquality() throws {
+        XCTAssertEqual(Money.dollar(5), Money.dollar(5))
+        XCTAssertNotEqual(Money.dollar(5), Money.dollar(6))
+        XCTAssertNotEqual(Money.franc(5), Money.dollar(5))
+    }
+
     func testCurrency() throws {
         XCTAssertEqual("USD", Money.dollar(1).currency())
         XCTAssertEqual("CHF", Money.franc(1).currency())
     }
+
     func testSimpleAddition() throws {
-        let sum = Money.dollar(5).plus(Money.dollar(5))
+        let five = Money.dollar(5)
+        let sum:Expression = five.plus(five)
         XCTAssertEqual(Money.dollar(10), sum)
+        let bank = Bank()
+        let reduced = bank.reduce(sum, "USD")
+        XCTAssertEqual(reduced, Money.dollar(10))
     }
 }
