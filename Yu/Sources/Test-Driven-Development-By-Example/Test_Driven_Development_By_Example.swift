fb21eb35157c2603fb8f6d0eb8978087037a1531
Tsungyu Yu <ytyubox@gmail.com>
2022-06-12 15:16:05 +0800

    ✅ refactor: kill duplicate of equals

--- Yu/Sources/Test-Driven-Development-By-Example/Test_Driven_Development_By_Example.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Test_Driven_Development_By_Example.swift
@@ -4,20 +4,14 @@ public class Money {
     public init(_ amount: Int) {
         self.amount = amount
     }
-
-    public func equals(_ object: Any) -> Bool {
-        let money = object as? Money
-        return amount == money?.amount
-        && type(of: self) == type(of: object)
-    }
 }
 
 extension Money: Equatable {
     public static func == (lhs: Money, rhs: Money) -> Bool {
-        lhs.equals(rhs)
+        lhs.amount == rhs.amount
+            && type(of: lhs) == type(of: rhs)
     }
 }
-
 
 public class Dollar: Money {
     public func times(_ mulitpler: Int) -> Dollar {
