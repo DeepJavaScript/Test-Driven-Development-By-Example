fdfd1ce3ec515db7afa6c5e530de511ceaf2f717
Tsungyu Yu <ytyubox@gmail.com>
2022-06-12 20:34:01 +0800

    ✅ refactor: no cast to sum

--- Yu/Sources/Test-Driven-Development-By-Example/Bank.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Bank.swift
@@ -2,7 +2,6 @@ public class Bank {
     public init() {}
     public func reduce(_ source: any Expression, _ to: String) -> Money {
         guard let sum = source as? Sum else {return Money(0, currency: to)}
-        let amount = sum.augend.amount + sum.addend.amount
-        return Money(amount, currency: to)
+        return sum.reduce(to)
     }
 }
--- Yu/Sources/Test-Driven-Development-By-Example/Expression.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Expression.swift
@@ -1,10 +1,14 @@
 public protocol Expression {}
-public class Sum:Expression {
-   public init(augend: Money, addend: Money) {
+public class Sum: Expression {
+    public init(augend: Money, addend: Money) {
         self.augend = augend
         self.addend = addend
     }
     
     public var augend: Money
     public var addend: Money
+    
+    func reduce(_ to: String) -> Money {
+        return Money(augend.amount + addend.amount, currency: to)
+    }
 }
