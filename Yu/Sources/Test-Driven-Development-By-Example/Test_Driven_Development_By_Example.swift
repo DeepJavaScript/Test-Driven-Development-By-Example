f0e3ffc8bb65fec26e53d6232c3e8a6eddf6c418
Tsungyu Yu <ytyubox@gmail.com>
2022-06-12 14:55:40 +0800

    common equals

--- Yu/Sources/Test-Driven-Development-By-Example/Test_Driven_Development_By_Example.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Test_Driven_Development_By_Example.swift
@@ -1,6 +1,36 @@
-public struct Test_Driven_Development_By_Example {
-    public private(set) var text = "Hello, World!"
+public class Money {
+    var amount: Int
 
-    public init() {
+    public init(_ amount: Int) {
+        self.amount = amount
+    }
+
+    public func equals(_ object: Any) -> Bool {
+        false
+    }
+}
+
+extension Money: Equatable {
+    public static func == (lhs: Money, rhs: Money) -> Bool {
+        lhs.equals(rhs)
+    }
+}
+
+
+public class Dollar: Money {
+    public func times(_ mulitpler: Int) -> Dollar {
+        Dollar(amount * mulitpler)
+    }
+    public override func equals(_ object: Any) -> Bool {
+        self.amount == (object as? Dollar)?.amount
+    }
+}
+
+public class Franc: Money {
+    public func times(_ mulitpler: Int) -> Franc {
+        Franc(amount * mulitpler)
+    }
+    public override func equals(_ object: Any) -> Bool {
+        self.amount == (object as? Franc)?.amount
     }
 }
