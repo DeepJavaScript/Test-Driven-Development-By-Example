5b31c34b3060506bd4a2bf54ab76ff39f370f486
Tsungyu Yu <ytyubox@gmail.com>
2022-06-12 20:37:57 +0800

    ✅ test pass by ugly if return

--- Yu/Sources/Test-Driven-Development-By-Example/Bank.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Bank.swift
@@ -1,6 +1,9 @@
 public class Bank {
     public init() {}
     public func reduce(_ source: any Expression, _ to: String) -> Money {
+        if let money = source as? Money {
+            return money
+        }
         guard let sum = source as? Sum else {return Money(0, currency: to)}
         return sum.reduce(to)
     }
