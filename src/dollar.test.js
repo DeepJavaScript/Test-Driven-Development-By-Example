124e70760fa65eb8b6ee434637d4ecc63d701cc4
cshif <cooshif@gmail.com>
2022-06-13 20:17:16 +0800

    rephrase first test's name

--- src/dollar.test.js
+++ src/dollar.test.js
@@ -1,10 +1,11 @@
 import { Dollar } from './dollar';
 
-test('5 美元 * 2 = 10 美元', () =>  {
+test('$5 * 2 = $10', () =>  {
   const five = new Dollar(5);
 
   let product = five.times(2);
   expect(product.amount).toBe(10);
+});
 
 test('equality of two value objects with same params should be true', () => {
   // toStrictEqual: test objects have same types and structure
