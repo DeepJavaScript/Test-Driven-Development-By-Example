7b4bb77e365f3ec71ecd9cf5a9280b136b845b1d
titangene <titangene.tw@gmail.com>
2022-06-16 17:23:14 +0800

    test: add dollar test describe block

--- titan/src/__tests__/dollar.test.ts
+++ titan/src/__tests__/dollar.test.ts
@@ -1,13 +1,15 @@
 import { Dollar } from '../dollar';
 
-it('multiplication', ( ) => {
-  const five: Dollar = new Dollar(5);
+describe('dollar test', () => {
+  it('multiplication', ( ) => {
+    const five: Dollar = new Dollar(5);
 
-  expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
-  expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
-});
+    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
+  });
 
-it('equals', () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+  it('equals', () => {
+    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+  });
 });
