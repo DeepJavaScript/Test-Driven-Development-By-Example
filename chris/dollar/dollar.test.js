7efdd90a6edb21cecce25312f52782d5df983a9e
chris <dwatow@gmail.com>
2022-06-16 09:21:34 +0800

    [o] modify amount not public

--- chris/dollar/dollar.test.js
+++ chris/dollar/dollar.test.js
@@ -5,13 +5,19 @@ describe('normal features', () => {
   it('test multiplication', () => {
     // prints name of the test
     const five = new Dollar(5)
-    let product = five.times(2);
+
+    // product = five.times(2)
     // assert.equal(10, product.amount)
-    assert.equal(true, new Dollar(10).equals(product))
+    assert.equal(true, new Dollar(10).equals(five.times(2)))
     assert.equal(true, new Dollar(10) == 10) // for JavaScript style
-    assert.equal(true, product == 10) // for JavaScript style
-    product = five.times(3)
-    assert.equal(15, product.amount)
+    assert.equal(true, five.times(2) == 10) // for JavaScript style
+    
+    // product = five.times(3)
+    // assert.equal(15, product.amount)
+    assert.equal(true, new Dollar(15).equals(five.times(3)))
+    assert.equal(true, new Dollar(15) == 15) // for JavaScript style
+    assert.equal(true, five.times(3) == 15) // for JavaScript style
+
   })
   it('test equals', () => {
     // prints name of the test
