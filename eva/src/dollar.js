cc5a5681268d4d97f6cb2d2b014965be1e1fa222
eva <cooshif@gmail.com>
2022-06-16 18:03:17 +0800

    refactor: made amount a private property, and renamed as total

--- eva/src/dollar.js
+++ eva/src/dollar.js
@@ -1,13 +1,19 @@
 export class Dollar {
+  #total;
+
   constructor(amount) {
-    this.amount = amount;
+    this.#total = amount;
   }
 
   times(multiplier) {
-    return new Dollar(this.amount * multiplier);
+    return new Dollar(this.#total * multiplier);
   }
 
   equals(obj)  {
-    return this.amount === obj.amount;
+    return this.#total === obj.amount;
+  }
+
+  get amount() {
+    return this.#total;
   }
 }
