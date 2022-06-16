c0bdd1656c4f3f5bdb3df475aea2b0e28fdf405b
eva <cooshif@gmail.com>
2022-06-16 08:49:43 +0800

    rewrite: compare Dollars

--- eva/src/dollar.test.js
+++ eva/src/dollar.test.js
@@ -1,10 +1,13 @@
 import { Dollar } from './dollar';
 
-test('$5 * 2 = $10', () =>  {
+test('test multiplication', () =>  {
   const five = new Dollar(5);
 
   let product = five.times(2);
   expect(product.amount).toBe(10);
+
+  const ten = new Dollar(10).amount;
+  expect(ten).toBe(product.amount);
 });
 
 test('equality of two value objects with same params should be true', () => {
