f8c585ce19f662635c02783c33e7f93f595b174d
titangene <titangene.tw@gmail.com>
2022-06-19 23:19:02 +0800

    feat: move Dollar equals method to Money for Ch6

--- titan/src/dollar.ts
+++ titan/src/dollar.ts
@@ -8,9 +8,4 @@ export class Dollar extends Money {
   times(multiplier: number): Dollar {
     return new Dollar(this.amount * multiplier);
   }
-
-  equals(object: object): boolean {
-    const dollar: Dollar = object as Dollar;
-    return this.amount === dollar.amount;
-  }
 }
--- titan/src/money.ts
+++ titan/src/money.ts
@@ -2,4 +2,9 @@ export class Money {
   constructor(protected amount: number) {
 
   }
+
+  equals(object: object): boolean {
+    const money: Money = object as Money;
+    return this.amount === money.amount;
+  }
 }
