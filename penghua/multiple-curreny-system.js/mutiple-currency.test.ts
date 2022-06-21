0213d5441b9e4fcd0edb83de63f76cc5ce60a2fd
penghuachen <kids5346@gmail.com>
2022-06-21 21:28:26 +0800

    feat: remove duplicate test

--- penghua/multiple-curreny-system.js/mutiple-currency.test.ts
+++ penghua/multiple-curreny-system.js/mutiple-currency.test.ts
@@ -4,26 +4,21 @@ describe("testMultiplication", () => {
     test("testMultiplication", () => {
 
         const five:Dollar = new Dollar(5);
-
         expect(new Dollar(10).equals(five.times(2))).toBeTruthy();
         expect(new Dollar(15).equals(five.times(3))).toBeTruthy();
+
+        const six:Franc = new Dollar(6);
+        expect(new Dollar(10).equals(six.times(2))).toBeTruthy();
+        expect(new Dollar(15).equals(six.times(3))).toBeTruthy();
     })
 
     test("testEquality", () => {
 
-        const five:Dollar = new Dollar(5);
-        expect(five.equals(five)).toBeTruthy();
+        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+        expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
 
-        const six:Dollar = new Dollar(6);
-        expect(five.equals(six)).toBeFalsy();
-    })
-
-    test("testFrancMultiplication", () => {
-
-        const five:Franc = new Franc(5);
-
-        expect(new Franc(10).equals(five.times(2))).toBeTruthy();
-        expect(new Franc(15).equals(five.times(3))).toBeTruthy();
+        expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
+        expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
     })
 })
 
