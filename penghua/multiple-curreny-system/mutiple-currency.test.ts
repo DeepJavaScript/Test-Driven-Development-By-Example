242608352ab9ef4dad371bd2a2bd038d74ede8aa
penghuachen <kids5346@gmail.com>
2022-06-21 21:49:49 +0800

    fix: testing case

--- penghua/multiple-curreny-system/mutiple-currency.test.ts
+++ penghua/multiple-curreny-system/mutiple-currency.test.ts
@@ -7,9 +7,9 @@ describe("testMultiplication", () => {
         expect(new Dollar(10).equals(five.times(2))).toBeTruthy();
         expect(new Dollar(15).equals(five.times(3))).toBeTruthy();
 
-        const six:Franc = new Dollar(6);
-        expect(new Dollar(10).equals(six.times(2))).toBeTruthy();
-        expect(new Dollar(15).equals(six.times(3))).toBeTruthy();
+        const six:Franc = new Franc(6);
+        expect(new Franc(12).equals(six.times(2))).toBeTruthy();
+        expect(new Franc(18).equals(six.times(3))).toBeTruthy();
     })
 
     test("testEquality", () => {
