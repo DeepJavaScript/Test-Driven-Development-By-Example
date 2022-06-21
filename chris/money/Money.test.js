f09f4a944b9cdd865d76d497a958e0513d38cfc7
chris <dwatow@gmail.com>
2022-06-21 10:23:15 +0800

    [o] but should be false

---
+++ chris/money/Money.test.js
@@ -0,0 +1,16 @@
+import { it, assert, describe } from 'vitest'
+import Dollar from './../dollar/Dollar'
+import Franc from './../franc/Franc'
+
+describe('dollar and franc test', () => {
+  it('test Franc and Dollar', () => {
+    assert.equal(true, new Dollar(5).equals(new Dollar(5)))
+    assert.equal(false, new Dollar(5).equals(new Dollar(6)))
+
+    assert.equal(true, new Franc(5).equals(new Franc(5)))
+    assert.equal(false, new Franc(5).equals(new Franc(6)))
+
+    // 匯率
+    assert.equal(true, new Franc(5).equals(new Dollar(5)))
+  })
+})
