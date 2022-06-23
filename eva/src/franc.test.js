e15d2ae781ec8941adf11d21f3aade81fbe0681d
eva <cooshif@gmail.com>
2022-06-23 19:17:11 +0800

    test Franc equality

--- eva/src/franc.test.js
+++ eva/src/franc.test.js
@@ -5,3 +5,8 @@ test('test Franc multiplication', () =>  {
   expect(new Franc(10).amount).toBe(five.times(2).amount);
   expect(new Franc(15).amount).toBe(five.times(3).amount);
 });
+
+test('equality of two value objects with same params should be true', () => {
+  expect(new Franc(5).equals(new Franc(5))).toBe(true);
+  expect(new Franc(5).equals(new Franc(6))).toBe(false);
+});
