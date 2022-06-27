54b68b349de8002fa49c97234a2dbf4f07ee51f3
penghuachen <kids5346@gmail.com>
2022-06-27 21:57:23 +0800

    refactor: use Money static dollar method instead

--- penghua/multiple-curreny-system/mutiple-currency.test.ts
+++ penghua/multiple-curreny-system/mutiple-currency.test.ts
@@ -3,8 +3,8 @@ import { Dollar, Franc, Money } from "./multiple-currency";
 describe("testMultiplication", () => {
     test("testMultiplication", () => {
         const five:Money = Money.dollar(5);
-        expect(new Dollar(10).equals(five.times(2))).toBeTruthy();
-        expect(new Dollar(15).equals(five.times(3))).toBeTruthy();
+        expect(Money.dollar(5).equals(five.times(2))).toBeTruthy();
+        expect(Money.dollar(5).equals(five.times(3))).toBeTruthy();
 
         const six:Franc = new Franc(6);
         expect(new Franc(12).equals(six.times(2))).toBeTruthy();
@@ -13,13 +13,13 @@ describe("testMultiplication", () => {
 
     test("testEquality", () => {
 
-        expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-        expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+        expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+        expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
 
         expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
         expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
 
-        expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
+        expect(new Franc(5).equals(Money.dollar(5))).toBeFalsy();
     })
 })
 
