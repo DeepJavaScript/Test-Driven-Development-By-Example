3db992dfbd25190b3f8d72a82570aba2455238d4
gene <marioreda3@gmail.com>
2022-06-30 13:12:01 +0800

    [x] ch7 add 5法郎與5美元 的比較測示

--- Gene/TDD-By-Example/Part1-Money/MoneyCase.cs
+++ Gene/TDD-By-Example/Part1-Money/MoneyCase.cs
@@ -29,6 +29,7 @@ namespace TDD_By_Example.Part1_Money
             Assert.IsFalse(new Dollar(5).Equals(new Dollar(6)));
             Assert.IsTrue(new Franc(5).Equals(new Franc(5)));
             Assert.IsFalse(new Franc(5).Equals(new Franc(6)));
+            Assert.IsFalse(new Franc(5).Equals(new Dollar(5)));
         }
     }
 }
