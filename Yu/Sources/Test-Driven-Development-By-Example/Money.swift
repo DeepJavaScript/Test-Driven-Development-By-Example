87184b428b677575d0b1e868253c5ace40d5257e
Tsungyu Yu <ytyubox@gmail.com>
2022-07-03 12:09:55 +0800

    format

--- Yu/Sources/Test-Driven-Development-By-Example/Money.swift
+++ Yu/Sources/Test-Driven-Development-By-Example/Money.swift
@@ -1,47 +1,47 @@
 public class Money: CustomDebugStringConvertible {
-    var amount: Int
-    var _currency: String
+  var amount: Int
+  var _currency: String
 
-    public init(_ amount: Int, currency: String) {
-        self.amount = amount
-        self._currency = currency
-    }
+  public init(_ amount: Int, currency: String) {
+    self.amount = amount
+    _currency = currency
+  }
 
-    public static func dollar(_ amount: Int) -> Money {
-        Money(amount, currency: "USD")
-    }
+  public static func dollar(_ amount: Int) -> Money {
+    Money(amount, currency: "USD")
+  }
 
-    public static func franc(_ amount: Int) -> Money {
-        Money(amount, currency: "CHF")
-    }
+  public static func franc(_ amount: Int) -> Money {
+    Money(amount, currency: "CHF")
+  }
 
-    public func times(_ mulitpler: Int) -> Money {
-        Money(amount * mulitpler, currency: _currency)
-    }
+  public func times(_ mulitpler: Int) -> Money {
+    Money(amount * mulitpler, currency: _currency)
+  }
 
-    public func currency() -> String {
-        _currency
-    }
+  public func currency() -> String {
+    _currency
+  }
 
-    public func plus(_ addend: Money) -> Expression {
-        Sum(augend: self, addend: addend)
-    }
+  public func plus(_ addend: Money) -> Expression {
+    Sum(augend: self, addend: addend)
+  }
 
-    public var debugDescription: String {
-        amount.description + " " + currency()
-    }
+  public var debugDescription: String {
+    amount.description + " " + currency()
+  }
 }
 
 extension Money: Equatable {
-    public static func == (lhs: Money, rhs: Money) -> Bool {
-        lhs.amount == rhs.amount
-            && lhs.currency() == rhs.currency()
-    }
+  public static func == (lhs: Money, rhs: Money) -> Bool {
+    lhs.amount == rhs.amount
+      && lhs.currency() == rhs.currency()
+  }
 }
 
 extension Money: Expression {
-    public func reduce(bank: Bank, _ to: String) -> Money {
-        let rate = bank.rate(currency(), to)
-        return Money(amount/rate, currency: to)
-    }
+  public func reduce(bank: Bank, _ to: String) -> Money {
+    let rate = bank.rate(currency(), to)
+    return Money(amount / rate, currency: to)
+  }
 }
