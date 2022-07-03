0efa4252244d3cc93f77b605670facde12ab0278
Tsungyu Yu <ytyubox@gmail.com>
2022-07-03 12:29:36 +0800

    move sum to its file

---
+++ Yu/Sources/Test-Driven-Development-By-Example/Sum.swift
@@ -0,0 +1,14 @@
+public class Sum: Expression {
+  public init(augend: Money, addend: Money) {
+    self.augend = augend
+    self.addend = addend
+  }
+
+  public var augend: Money
+  public var addend: Money
+
+  public func reduce(bank: Bank, _ to: String) -> Money {
+    let amount: Int = augend.reduce(bank: bank, to).amount + addend.reduce(bank: bank, to).amount
+    return Money(amount, currency: to)
+  }
+}
