cc0c76aef15593bfdc4adebd5a11c1cac61fa480
chris <dwatow@gmail.com>
2022-07-05 12:25:30 +0800

    [o] add money/MoneyFactory.js

--- chris/money/Money.js
+++ chris/money/Money.js
@@ -1,19 +1,8 @@
+
 export default class Money {
   #amount
   constructor(amount) {
     this.#amount = amount
-    
-    // protect is 延伸類別的私有屬性, but 這麼做變 public，只是共用了 #amount
-    // if (!('amount' in this)) {
-    //   Object.defineProperty(Money.prototype, "amount", {
-    //     set(value) {
-    //       this.#amount = value
-    //     },
-    //     get() {
-    //       return this.#amount
-    //     },
-    //   });
-    // }
     if (!('getClass' in this)) {
       Object.defineProperty(Money.prototype, "getClass", {
         get() {
@@ -30,3 +19,4 @@ export default class Money {
     return this.#amount
   }
 }
+
