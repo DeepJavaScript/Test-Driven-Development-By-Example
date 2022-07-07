81aaee2043f7afcd4a9e55e298c1b4a51062afd1
titangene <titangene.tw@gmail.com>
2022-07-07 20:13:30 +0800

    [o] refactor(ch10): inline factory method in Franc and Dollar times()

--- titan/src/money.ts
+++ titan/src/money.ts
@@ -29,7 +29,7 @@ export class Dollar extends Money {
   }
 
   times(multiplier: number): Money {
-    return Money.dollar(this.amount * multiplier);
+    return new Dollar(this.amount * multiplier, 'USD');
   }
 }
 
@@ -39,6 +39,6 @@ export class Franc extends Money {
   }
 
   times(multiplier: number): Money {
-    return Money.franc(this.amount * multiplier);
+    return new Franc(this.amount * multiplier, 'CHF');
   }
 }
