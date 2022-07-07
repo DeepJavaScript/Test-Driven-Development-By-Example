a6badf5069b058d0dd2591c4a93c9b8585da565a
titangene <titangene.tw@gmail.com>
2022-07-07 16:54:00 +0800

    [x] test: change from `new Dollar` to `Money.dollar`

--- titan/src/__tests__/money.test.ts
+++ titan/src/__tests__/money.test.ts
@@ -6,8 +6,8 @@ describe('money test', () => {
   it('dollar multiplication', () => {
     const five: Money = Money.dollar(5);
 
-    expect(five.times(2).equals(new Dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(new Dollar(15))).toBeTruthy();
+    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
+    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
   });
 
   it('franc multiplication', () => {
@@ -18,12 +18,12 @@ describe('money test', () => {
   });
 
   it('equals', () => {
-    expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-    expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+    expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+    expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
 
     expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
     expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
 
-    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
+    expect(new Franc(5).equals(Money.dollar(5))).toBeFalsy();
   });
 });
