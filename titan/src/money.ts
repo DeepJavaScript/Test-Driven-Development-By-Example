a388629306133ce7278980bd24cd36e28011888e
titangene <titangene.tw@gmail.com>
2022-07-07 17:07:45 +0800

    [o] fix: move Dollar and Franc into Money file to fix circular dependencies
    TypeError: Class extends value undefined is not a constructor or null

--- titan/src/money.ts
+++ titan/src/money.ts
@@ -1,6 +1,3 @@
-import { Dollar } from './dollar';
-import { Franc } from './franc';
-
 export abstract class Money {
   static franc(amount: number): Money {
     return new Franc(amount);
@@ -19,5 +16,25 @@ export abstract class Money {
     return (
       this.amount === money.amount && money.constructor === this.constructor
     );
+  }
+}
+
+export class Dollar extends Money {
+  constructor(public amount: number) {
+    super();
+  }
+
+  times(multiplier: number): Money {
+    return new Dollar(this.amount * multiplier);
+  }
+}
+
+export class Franc extends Money {
+  constructor(public amount: number) {
+    super();
+  }
+
+  times(multiplier: number): Money {
+    return new Franc(this.amount * multiplier);
   }
 }
