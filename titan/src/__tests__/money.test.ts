d5e31c88fb09b0a2a3e2356e9c2a63f561d35c25
titangene <titangene.tw@gmail.com>
2022-07-07 16:56:51 +0800

    [x] test: change from `new Franc` to `Money.franc`

--- titan/src/__tests__/money.test.ts
+++ titan/src/__tests__/money.test.ts
@@ -1,5 +1,3 @@
-import { Dollar } from '../dollar';
-import { Franc } from '../franc';
 import { Money } from '../money';
 
 describe('money test', () => {
@@ -11,19 +9,19 @@ describe('money test', () => {
   });
 
   it('franc multiplication', () => {
-    const five: Franc = new Franc(5);
+    const five: Money = Money.franc(5);
 
-    expect(five.times(2).equals(new Franc(10))).toBeTruthy();
-    expect(five.times(3).equals(new Franc(15))).toBeTruthy();
+    expect(five.times(2).equals(Money.franc(10))).toBeTruthy();
+    expect(five.times(3).equals(Money.franc(15))).toBeTruthy();
   });
 
   it('equals', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
 
-    expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-    expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
+    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
 
-    expect(new Franc(5).equals(Money.dollar(5))).toBeFalsy();
+    expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
   });
 });
