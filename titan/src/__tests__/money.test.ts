2874c3d641e8a7082710458a1b3b6bab56721d2a
titangene <titangene.tw@gmail.com>
2022-07-07 14:27:16 +0800

    [x] test: add franc & dollar compare

--- titan/src/__tests__/money.test.ts
+++ titan/src/__tests__/money.test.ts
@@ -19,7 +19,10 @@ describe('money test', () => {
   it('equals', () => {
     expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
     expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
+
     expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
     expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+
+    expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
   });
 });
