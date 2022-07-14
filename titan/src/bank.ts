77e3031b62d7aec82febff0d6450c14023b72c34
titangene <titangene.tw@gmail.com>
2022-07-14 19:25:24 +0800

    [o] feat(ch13): complete reduce() when same currency

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -1,8 +1,11 @@
 import { Expression } from './expression';
 import { Money } from './money';
+import { Sum } from './sum';
 
 export class Bank {
   reduce(source: Expression, to: string): Money {
-    return Money.dollar(10);
+    const sum: Sum = source as Sum;
+    const amount: number = sum.augend.amount + sum.addend.amount;
+    return new Money(amount, to);
   }
 }
