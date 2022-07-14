464421769ed6e864e0b92ceaf0a43f19616fe245
titangene <titangene.tw@gmail.com>
2022-07-14 20:27:50 +0800

    [o] feat(ch13): Bank reduce() hard-code if condition only for Money type

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -4,6 +4,9 @@ import { Sum } from './sum';
 
 export class Bank {
   reduce(source: Expression, to: string): Money {
+    if (source instanceof Money) {
+      return source as Money;
+    }
     const sum: Sum = source as Sum;
     return sum.reduce(to);
   }
