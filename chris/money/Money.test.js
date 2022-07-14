c13f2d4ba58ab419e535d7e61133e12793bc2c67
chris <dwatow@gmail.com>
2022-07-14 20:01:50 +0800

    remove repeat test

--- chris/money/Money.test.js
+++ chris/money/Money.test.js
@@ -15,21 +15,6 @@ describe('dollar and franc test', () => {
     const five = MoneyFactory.franc(5);
     assert.equal(false, MoneyFactory.franc(5).equals(MoneyFactory.dollar(5)))
   })
-  it('testCurrency', () => {
-    assert.equal("USD", MoneyFactory.dollar(5).currency())
-    assert.equal("CHF", MoneyFactory.franc(5).currency())
-  })
-})
-
-describe('dollar test', () => {
-  it('test multiplication', () => {
-    const five = MoneyFactory.dollar(5)
-    assert.equal(true, MoneyFactory.dollar(10).equals(five.times(2)), `${MoneyFactory.dollar(10)} ≠ ${five.times(2)}`)
-    assert.equal(true, five.times(2) == 10) // for JavaScript style
-
-    assert.equal(true, MoneyFactory.dollar(15).equals(five.times(3)))
-    assert.equal(true, five.times(3) == 15) // for JavaScript style
-  })
 
   it('test equals as integer', () => {
     assert.equal(true, MoneyFactory.dollar(10) == 10) // for JavaScript style
@@ -37,30 +22,15 @@ describe('dollar test', () => {
     assert.equal(true, MoneyFactory.dollar(15) == 15) // for JavaScript style
     assert.equal(true, MoneyFactory.dollar(15) != 15 - 1) // for JavaScript style
   })
-  it('test equals as Object the same Class', () => {
-    assert.equal(true, MoneyFactory.dollar(5).equals(MoneyFactory.dollar(5)));
-    assert.equal(false, MoneyFactory.dollar(5).equals(MoneyFactory.dollar(6)));
-  })
-})
 
-describe('franc test', () => {
   it('test multiplication', () => {
-    const five = MoneyFactory.franc(5)
-    assert.equal(true, MoneyFactory.franc(10).equals(MoneyFactory.franc(10)))
-    assert.equal(true, five.times(2) == 10) // for JavaScript style
-
-    assert.equal(true, MoneyFactory.franc(15).equals(MoneyFactory.franc(15)))
-    assert.equal(true, five.times(3) == 15) // for JavaScript style
+    const five = MoneyFactory.dollar(5)
+    assert.equal(true, MoneyFactory.dollar(5).times(2) == 10) // for JavaScript style
+    assert.equal(true, MoneyFactory.dollar(5).times(3) == 15) // for JavaScript style
   })
 
-  it('test equals as integer', () => {
-    assert.equal(true, MoneyFactory.franc(10) == 10) // for JavaScript style
-    assert.equal(true, MoneyFactory.franc(10) != 10 + 1) // for JavaScript style
-    assert.equal(true, MoneyFactory.franc(15) == 15) // for JavaScript style
-    assert.equal(true, MoneyFactory.franc(15) != 15 - 1) // for JavaScript style
-  })
-  it('test equals as Object the same Class', () => {
-    assert.equal(true, MoneyFactory.franc(5).equals(MoneyFactory.franc(5)));
-    assert.equal(false, MoneyFactory.franc(5).equals(MoneyFactory.franc(6)));
+  it('testCurrency', () => {
+    assert.equal("USD", MoneyFactory.dollar(5).currency())
+    assert.equal("CHF", MoneyFactory.franc(5).currency())
   })
 })
