6473408bae3b62922ad86b9033216e72d086cfb9
titangene <titangene.tw@gmail.com>
2022-07-14 16:33:26 +0800

    [o] feat(ch12): create Bank reduce() and hard-code return Money.dollar(10)

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -1,1 +1,8 @@
-export class Bank {}
+import { Expression } from './expression';
+import { Money } from './money';
+
+export class Bank {
+  reduce(source: Expression, to: string): Money {
+    return Money.dollar(10);
+  }
+}
