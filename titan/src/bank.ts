41f7b12eacb60953e57c320bbe56f7d0a6c26eec
titangene <titangene.tw@gmail.com>
2022-07-14 20:19:42 +0800

    [o] refactor(ch13): move reduce() algorithm to Sum.reduce()

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -5,7 +5,6 @@ import { Sum } from './sum';
 export class Bank {
   reduce(source: Expression, to: string): Money {
     const sum: Sum = source as Sum;
-    const amount: number = sum.augend.amount + sum.addend.amount;
-    return new Money(amount, to);
+    return sum.reduce(to);
   }
 }
--- titan/src/sum.ts
+++ titan/src/sum.ts
@@ -3,4 +3,9 @@ import { Money } from './money';
 
 export class Sum implements Expression {
   constructor(public augend: Money, public addend: Money) {}
+
+  reduce(to: string): Money {
+    const amount: number = this.augend.amount + this.addend.amount;
+    return new Money(amount, to);
+  }
 }
