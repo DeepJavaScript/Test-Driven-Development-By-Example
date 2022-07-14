2eba791a97d3dda08cf46b464f8e4d8073f57f7e
titangene <titangene.tw@gmail.com>
2022-07-14 14:21:34 +0800

    [o] test(ch11): remove duplicate meaning assertions from equals test case

--- titan/src/__tests__/money.test.ts
+++ titan/src/__tests__/money.test.ts
@@ -19,9 +19,6 @@ describe('money test', () => {
     expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
     expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
 
-    expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
-    expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
-
     expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
   });
 
