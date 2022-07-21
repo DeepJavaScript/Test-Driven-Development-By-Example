f39970f0c9ff702d2852b8cd3cc369bf588bddcb
titangene <titangene.tw@gmail.com>
2022-07-21 17:02:42 +0800

    [o] refactor: extract expectMoneyEquals() in test file

--- titan/src/__tests__/money.test.ts
+++ titan/src/__tests__/money.test.ts
@@ -3,19 +3,23 @@ import { Expression } from '../expression';
 import { Money } from '../money';
 import { Sum } from '../sum';
 
+function expectMoneyEquals(actual: Expression, expected: Expression) {
+  expect((actual as Money).equals(expected as Money)).toBeTruthy();
+}
+
 describe('money test', () => {
   it('dollar multiplication', () => {
     const five: Money = Money.dollar(5);
 
-    expect(five.times(2).equals(Money.dollar(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.dollar(15))).toBeTruthy();
+    expectMoneyEquals(five.times(2), Money.dollar(10));
+    expectMoneyEquals(five.times(3), Money.dollar(15));
   });
 
   it('franc multiplication', () => {
     const five: Money = Money.franc(5);
 
-    expect(five.times(2).equals(Money.franc(10))).toBeTruthy();
-    expect(five.times(3).equals(Money.franc(15))).toBeTruthy();
+    expectMoneyEquals(five.times(2), Money.franc(10));
+    expectMoneyEquals(five.times(3), Money.franc(15));
   });
 
   it('equals', () => {
@@ -35,35 +39,35 @@ describe('money test', () => {
     const sum: Expression = five.plus(five);
     const bank: Bank = new Bank();
     const reduced: Money = bank.reduce(sum, 'USD');
-    expect(reduced.equals(Money.dollar(10))).toBeTruthy();
+    expectMoneyEquals(reduced, Money.dollar(10));
   });
 
   it('plus returns sum', () => {
     const five: Money = Money.dollar(5);
     const result: Expression = five.plus(five);
     const sum: Sum = result as Sum;
-    expect(five.equals(sum.augend)).toBeTruthy();
-    expect(five.equals(sum.addend)).toBeTruthy();
+    expectMoneyEquals(five, sum.augend);
+    expectMoneyEquals(five, sum.addend);
   });
 
   it('reduce sum', () => {
     const sum: Expression = new Sum(Money.dollar(3), Money.dollar(4));
     const bank: Bank = new Bank();
     const result: Money = bank.reduce(sum, 'USD');
-    expect(result.equals(Money.dollar(7))).toBeTruthy();
+    expectMoneyEquals(result, Money.dollar(7));
   });
 
   it('reduce money', () => {
     const bank: Bank = new Bank();
     const result: Money = bank.reduce(Money.dollar(1), 'USD');
-    expect(result.equals(Money.dollar(1))).toBeTruthy();
+    expectMoneyEquals(result, Money.dollar(1));
   });
 
   it('reduce money different currency', () => {
     const bank: Bank = new Bank();
     bank.addRate('CHF', 'USD', 2);
     const result: Money = bank.reduce(Money.franc(2), 'USD');
-    expect(Money.dollar(1).equals(result)).toBeTruthy();
+    expectMoneyEquals(result, Money.dollar(1));
   });
 
   it('mixed addition', () => {
@@ -72,6 +76,6 @@ describe('money test', () => {
     const bank: Bank = new Bank();
     bank.addRate('CHF', 'USD', 2);
     const result: Money = bank.reduce(fiveBucks.plus(tenFrancs), 'USD');
-    expect(Money.dollar(10).equals(result)).toBeTruthy();
+    expectMoneyEquals(result, Money.dollar(10));
   });
 });
