94e124a79c59a3639508276f8c37a94174874137
titangene <titangene.tw@gmail.com>
2022-07-21 15:58:02 +0800

    [x] feat(ch14): create RateMap

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -2,6 +2,8 @@ import { Expression } from './expression';
 import { Money } from './money';
 
 export class Bank {
+  private rates: RateMap = new RateMap();
+
   reduce(source: Expression, to: string): Money {
     return source.reduce(this, to);
   }
@@ -19,5 +21,19 @@ class Pair {
 
   hashCode(): number {
     return 0;
+  }
+}
+
+class RateMap extends Map<Pair, number> {
+  constructor(iterable?: Iterable<[Pair, number]>) {
+    super(iterable);
+  }
+
+  get(pair: Pair): number {
+    const entries: [Pair, number][] = [...this.entries()];
+    const entry: [Pair, number] | undefined = entries.find(([_pair]) =>
+      _pair.equals(pair)
+    );
+    return entry === undefined ? 1 : entry[1];
   }
 }
