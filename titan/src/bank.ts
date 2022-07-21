9a123667de5c4aa2a61008210528b8d3cbc47c2b
titangene <titangene.tw@gmail.com>
2022-07-21 14:30:18 +0800

    [o] refactor(ch13): remove useless if condition by polymorphism as Expression

--- titan/src/bank.ts
+++ titan/src/bank.ts
@@ -4,10 +4,6 @@ import { Sum } from './sum';
 
 export class Bank {
   reduce(source: Expression, to: string): Money {
-    if (source instanceof Money) {
-      return (source as Money).reduce(to);
-    }
-    const sum: Sum = source as Sum;
-    return sum.reduce(to);
+    return source.reduce(to);
   }
 }
