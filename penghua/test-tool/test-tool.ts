413583af5be79e6ac323edb9d3039d8b9324956a
penghuachen <kids5346@gmail.com>
2022-07-30 17:31:13 +0800

    feat: create parent class TestCase and move run fn and name property into parent class

--- penghua/test-tool/test-tool.ts
+++ penghua/test-tool/test-tool.ts
@@ -1,15 +1,21 @@
-class WasRun {
-    public wasRun = null;
+class TestCase {
     constructor(public name: string) {
-    }
-
-    testMethod() {
-        this.wasRun = true;
     }
 
     run() {
         // 取得 name 的值作為 method name 並執行
         this[this.name]();
+    }
+}
+
+class WasRun extends TestCase {
+    constructor(props) {
+        super(props);
+    }
+    public wasRun = null;
+
+    testMethod() {
+        this.wasRun = true;
     }
 }
 
