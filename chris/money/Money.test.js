5027b8167097e1868e82f00268b3b118fee7a53c
chris <dwatow@gmail.com>
2022-08-05 19:44:57 +0800

    [x] add testSumPlusMoney

--- chris/money/Money.test.js
+++ chris/money/Money.test.js
@@ -72,13 +72,13 @@ describe('dollar and franc test', () => {
     assert.equal(true, MoneyFactory.dollar(10).equals(result), `${MoneyFactory.dollar(15)} ≠ ${result}`);
   })
 
-  // it('testSumPlusMoney', () => {
-  //   const fiveBucks = MoneyFactory.dollar(5);
-  //   const tenFrances = MoneyFactory.franc(10);
-  //   const bank = new Bank();
-  //   bank.addRate('CHF', 'USD', 2);
-  //   const sum = new Sum(fiveBucks, tenFrances).plus(fiveBucks);
-  //   const result = bank.reduce(sum, 'USD');
-  //   assert.equal(true, MoneyFactory.dollar(15).equals(result), `${MoneyFactory.dollar(15)} ≠ ${result}`);
-  // })
+  it('testSumPlusMoney', () => {
+    const fiveBucks = MoneyFactory.dollar(5);
+    const tenFrances = MoneyFactory.franc(10);
+    const bank = new Bank();
+    bank.addRate('CHF', 'USD', 2);
+    const sum = new Sum(fiveBucks, tenFrances).plus(fiveBucks);
+    const result = bank.reduce(sum, 'USD');
+    assert.equal(true, MoneyFactory.dollar(15).equals(result), `${MoneyFactory.dollar(15)} ≠ ${result}`);
+  })
 })
