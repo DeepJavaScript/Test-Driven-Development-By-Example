d6f1ee22c119e30a8a0369c1d2c3fc5a98cfbdae
penghuachen <kids5346@gmail.com>
2022-08-08 00:05:53 +0800

    feat: add setUp fn and refactor testRunning and testSetUp fn

--- penghua/test-tool/test-tool.ts
+++ penghua/test-tool/test-tool.ts
@@ -2,14 +2,20 @@ class TestCase {
     constructor(public name: string) {
     }
 
+    // run  只會用到 name，作者認為是屬於父類別的職責
     run(): void {
+        this.setUp();
         // 取得 name 的值作為 method name 並執行
         this[this.name]();
+    }
+
+    setUp(): void {
     }
 }
 
 class WasRun extends TestCase {
     public wasRun = null;
+    public wasSetup = null;
 
     constructor(props) {
         super(props);
@@ -18,18 +24,36 @@ class WasRun extends TestCase {
     testMethod(): void {
         this.wasRun = true;
     }
-}
 
-class TestCaseTest {
-    static testRunning(): void {
-       const test: WasRun = new WasRun("testMethod");
-        console.assert(test.wasRun === null);
-        test.run();
-        console.assert(test.wasRun === true);
+    setUp(): void {
+        this.wasRun = true;
+        this.wasSetup = true;
     }
 }
 
+class TestCaseTest {
+    static test: WasRun = null;
+
+    static setUp(): void {
+      this.test = new WasRun("testMethod");
+    }
+
+    static testRunning(): void {
+        // const test: WasRun = new WasRun("testMethod");
+        this.test.run();
+        console.assert(this.test.wasRun === true);
+    }
+
+    static testSetUp(): void {
+        // const test: WasRun = new WasRun("testMethod");
+        this.test.run();
+        console.assert(this.test.wasSetup === true);
+    }
+}
+
+TestCaseTest.setUp();
 TestCaseTest.testRunning();
+TestCaseTest.testSetUp();
 export {
     WasRun
 }
