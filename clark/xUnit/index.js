af1cb0a042c94e055b9d41fc8039cb3e7bc13928
GQSM <ms314006@hotmail.com>
2022-08-21 13:44:44 +0800

    ADD 完成 TestResult 和他的測試案例

--- clark/xUnit/index.js
+++ clark/xUnit/index.js
@@ -7,9 +7,16 @@ class TestCase {
   tearDown() {}
 
   run() {
+    const result = new TestResult();
+    result.testStarted();
     this.setUp();
-    this[this.name]();
+    try {
+      this[this.name]();
+    } catch (e) {
+      result.testBroken();
+    }
     this.tearDown();
+    return result;
   }
 }
 
@@ -31,9 +38,32 @@ class WasRun extends TestCase {
     this.log = `${this.log}testMethod `;
   }
 
+  testBrokenMethod() {
+    throw 'error';
+  }
+
   tearDown() {
     this.wasRun = true;
     this.log = `${this.log}tearDown `;
+  }
+}
+
+class TestResult {
+  constructor() {
+    this.runCount = 0;
+    this.failedCount = 0;
+  }
+
+  get summary() {
+    return `${this.runCount} run, ${this.failedCount} failed`;
+  }
+
+  testStarted() {
+    this.runCount += 1;
+  }
+
+  testBroken() {
+    this.failedCount += 1;
   }
 }
 
@@ -47,6 +77,20 @@ class TestCaseTest extends TestCase {
     test.run();
     console.assert(test.log === 'setUp testMethod tearDown ');
   }
+
+  testResult() {
+    const test = new WasRun('testMethod');
+    const result = test.run();
+    console.assert('1 run, 0 failed' === result.summary);
+  }
+
+  testFailedResult() {
+    const test = new WasRun('testBrokenMethod');
+    const result = test.run();
+    console.assert('1 run, 1 failed' === result.summary);
+  }
 }
 
 new TestCaseTest('testTemplateMethod').run();
+new TestCaseTest('testResult').run();
+new TestCaseTest('testFailedResult').run();
