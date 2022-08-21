2c9840f2916c57fbb7c7b7fe01fbbd9fd5ca8cd4
GQSM <ms314006@hotmail.com>
2022-08-21 13:27:43 +0800

    Add tearDown and rename test cast name to testTemplateMethod

--- clark/xUnit/index.js
+++ clark/xUnit/index.js
@@ -4,10 +4,12 @@ class TestCase {
   }
 
   setUp() {}
+  tearDown() {}
 
   run() {
     this.setUp();
     this[this.name]();
+    this.tearDown();
   }
 }
 
@@ -16,38 +18,35 @@ class WasRun extends TestCase {
     super(name);
     this.wasRun = null;
     this.wasWetUp = null;
-  }
-  
-  testMethod() {
-    this.wasRun = true;
+    this.log = 'setUp ';
   }
 
   setUp() {
     this.wasRun = null;
     this.wasWetUp = true;
   }
+
+  testMethod() {
+    this.wasRun = true;
+    this.log = `${this.log}testMethod `;
+  }
+
+  tearDown() {
+    this.wasRun = true;
+    this.log = `${this.log}tearDown `;
+  }
 }
 
 class TestCaseTest extends TestCase {
   constructor(name) {
     super(name);
-    this.test = null;
   }
 
-  setUp() {
-    this.test = new WasRun('testMethod');
-  }
-
-  testRunning() {
-    this.test.run();
-    console.assert(this.test.wasRun === true);
-  }
-
-  testSetUp() {
-    this.test.run();
-    console.assert(this.test.wasWetUp === true);
+  testTemplateMethod() {
+    const test = new WasRun('testMethod');
+    test.run();
+    console.assert(test.log === 'setUp testMethod tearDown ');
   }
 }
 
-new TestCaseTest('testRunning').run();
-new TestCaseTest('testSetUp').run();
+new TestCaseTest('testTemplateMethod').run();
