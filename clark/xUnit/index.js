5c5732b0b6ada72cf1f3582c4732a533c7f0b1f0
GQSM <ms314006@hotmail.com>
2022-08-25 20:47:34 +0800

    Add teardown always run and catch set up error

--- clark/xUnit/index.js
+++ clark/xUnit/index.js
@@ -8,8 +8,8 @@ class TestCase {
 
   run(result) {
     result.testStarted();
-    this.setUp();
     try {
+      this.setUp();
       this[this.name]();
     } catch (e) {
       result.testFailed();
@@ -24,12 +24,13 @@ class WasRun extends TestCase {
     super(name);
     this.wasRun = null;
     this.wasWetUp = null;
-    this.log = 'setUp ';
+    this.log = '';
   }
 
   setUp() {
     this.wasRun = null;
     this.wasWetUp = true;
+    this.log = 'setUp ';
   }
 
   testMethod() {
@@ -96,25 +97,31 @@ class TestCaseTest extends TestCase {
   testTemplateMethod() {
     const test = new WasRun('testMethod');
     test.run(this.result);
-    console.assert(test.log === 'setUp testMethod tearDown ');
+    console.assert(test.log === 'setUp testMethod tearDown ', 'testTemplateMethod');
+  }
+
+  testTearDownAfterFailedResult() {
+    const test = new WasRun('testBrokenMethod');
+    test.run(this.result);
+    console.assert(test.log === 'setUp tearDown ', 'testTearDownAfterFailedResult');
   }
 
   testResult() {
     const test = new WasRun('testMethod');
     test.run(this.result);
-    console.assert('1 run, 0 failed' === this.result.summary);
+    console.assert('1 run, 0 failed' === this.result.summary, 'testResult');
   }
 
   testFailedResult() {
     const test = new WasRun('testBrokenMethod');
     test.run(this.result);
-    console.assert('1 run, 1 failed' === this.result.summary);
+    console.assert('1 run, 1 failed' === this.result.summary, 'testFailedResult');
   }
 
   testFailedResultFormatting() {
     this.result.testStarted();
     this.result.testFailed();
-    console.assert('1 run, 1 failed' === this.result.summary);
+    console.assert('1 run, 1 failed' === this.result.summary, 'testFailedResultFormatting');
   }
 
   testSuite() {
@@ -122,12 +129,14 @@ class TestCaseTest extends TestCase {
     suite.add(new WasRun('testMethod'));
     suite.add(new WasRun('testBrokenMethod'));
     suite.run(this.result);
-    console.assert('2 run, 1 failed' === this.result.summary);
+    console.assert('2 run, 1 failed' === this.result.summary, 'testSuite');
   }
 }
 
+console.log('====TestCaseTest====');
 const suite = new TestSuite();
 suite.add(new TestCaseTest('testTemplateMethod'));
+suite.add(new TestCaseTest('testTearDownAfterFailedResult'));
 suite.add(new TestCaseTest('testResult'));
 suite.add(new TestCaseTest('testFailedResult'));
 suite.add(new TestCaseTest('testFailedResultFormatting'));
@@ -135,3 +144,35 @@ suite.add(new TestCaseTest('testSuite'));
 const result = new TestResult();
 suite.run(result);
 console.log(result.summary);
+
+
+console.log('====TestSetUpFailedTest====');
+
+class TestSetUpFailedTest extends TestCase {
+  constructor(name) {
+    super(name);
+    this.result = null;
+  }
+
+  setUp() {
+    throw 'error';
+  }
+
+  testTemplateMethod() {
+    const test = new WasRun('testMethod', 'testTemplateMethod');
+    test.run(this.result);
+  }
+
+  testTearDownAfterFailedResult() {
+    const test = new WasRun('testBrokenMethod');
+    test.run(this.result);
+    console.assert(test.log === 'setUp tearDown ', 'testTearDownAfterFailedResult');
+  }
+}
+
+const setUpFailedTestSuite = new TestSuite();
+setUpFailedTestSuite.add(new TestSetUpFailedTest('testTemplateMethod'));
+setUpFailedTestSuite.add(new TestSetUpFailedTest('testTearDownAfterFailedResult'));
+const testSetUpFailedResult = new TestResult();
+setUpFailedTestSuite.run(testSetUpFailedResult);
+console.log(testSetUpFailedResult.summary);
