3dc8c07c547a8bc0eaa9368c41a923cedba297ec
titangene <titangene.tw@gmail.com>
2022-08-25 20:07:04 +0800

    [x] feat(ch23): change main code

--- titan/xunit/src/main.ts
+++ titan/xunit/src/main.ts
@@ -118,8 +118,13 @@ class TestCaseTest extends TestCase {
   }
 }
 
-new TestCaseTest('testTemplateMethod').run();
-new TestCaseTest('testResult').run();
-new TestCaseTest('testFailedResult').run();
-new TestCaseTest('testFailedResultFormatting').run();
-new TestCaseTest('testSuite').run();
+const suite = new TestSuite();
+suite.add(new TestCaseTest('testTemplateMethod'));
+suite.add(new TestCaseTest('testResult'));
+suite.add(new TestCaseTest('testFailedResult'));
+suite.add(new TestCaseTest('testFailedResultFormatting'));
+suite.add(new TestCaseTest('testSuite'));
+
+const result = new TestResult();
+suite.run(result);
+console.log(result.summary());
